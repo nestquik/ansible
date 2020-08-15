205b9723e9d2e921ff707c172c4460b7f6a6cd9c
nestquik <69665625+nestquik@users.noreply.github.com>
2020-08-15 06:46:05 -0700

    Create authconfig.py

---
+++ scripts/authconfig.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from subprocess import call, PIPE, Popen
+
+out = Popen(['authconfig', '--test'], stdout=PIPE)
+out_list = out.communicate()[0].split('\n')
+
+
+hash_ = [i for i, string in enumerate(out_list) if 'ALGORITHM' in string.upper()]
+
+
+for i in hash_:
+        hash = out_list[i].split(' ')[-1]
+        print hash
+        with open("/tmp/restore_passhash.py", "w") as f:
+                f.write("#!/usr/bin/env python")
+                f.write('\n')
+                f.write("from subprocess import call")
+                f.write('\n')
+                f.write("call([\"authconfig\", \"--passalgo=\",")
+                f.write(hash+", \"--update\"])")
