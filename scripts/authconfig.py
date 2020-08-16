7f12db3c69c7af09539fe9946d9044ad1c6f8d8b
nestquik <69665625+nestquik@users.noreply.github.com>
2020-08-15 22:12:09 -0700

    Update authconfig.py

--- scripts/authconfig.py
+++ scripts/authconfig.py
@@ -10,12 +10,11 @@ hash_ = [i for i, string in enumerate(out_list) if 'ALGORITHM' in string.upper()
 
 
 for i in hash_:
-        hash = out_list[i].split(' ')[-1]
-        print hash
-        with open("/tmp/restore_passhash.py", "w") as f:
-                f.write("#!/usr/bin/env python")
-                f.write('\n')
-                f.write("from subprocess import call")
-                f.write('\n')
-                f.write("call([\"authconfig\", \"--passalgo=\",")
-                f.write(hash+", \"--update\"])")
+	hash = out_list[i].split(' ')[-1]
+	with open("/tmp/restore_passhash.py", "w") as f:
+		f.write("#!/usr/bin/env python")
+		f.write('\n')
+		f.write("from subprocess import call")
+		f.write('\n')
+                auth_str = "call([\"authconfig\", \"--passalgo="+hash+"\", \""+"--update\"])"
+		f.write(auth_str)
